638fe06162e6416a50de7edab0f98c923630f3f8
Lars Siemon <github@larssiemon.de>
2022-03-06 13:47:00 +0100

    Added License note - License header for TimedRewards

--- TimedRewards/TimedRewards.cs
+++ TimedRewards/TimedRewards.cs
@@ -1,4 +1,25 @@
-﻿using System.Collections.Generic;
+﻿/*
+ * https://github.com/DocValerian/rust-plugins
+ * Copyright (C) 2022 DocValerian
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Questions and Support can be found in our Discord: https://discord.gg/8tkBFRjg3W
+ * 
+ */
+
+using System.Collections.Generic;
 using Oxide.Core;
 using Oxide.Core.Plugins;
 using UnityEngine;
